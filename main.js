09644c625a2d67216e968d68ce8de50d7890eaa7
Marc <marcdsp@gmail.com>
2023-01-17 14:00:56 -0600

    percentage function implemented

--- main.js
+++ main.js
@@ -1,168 +1,332 @@
-let currentNumber = []; //use document query string???
+let currentNumber = [];
+let currentNumberDisplay = []; //use document query string???
 let previousNumber = [];
+let previousNumberDisplay = [];
 let runningCalculation = [];
 let calculationStarted = false;
 let previousResult = null;
 let lastOperator = null; //might not need this
-let theKeyPad=document.querySelector('#keyPanel');
+let theKeyPad = document.querySelector("#keyPanel");
 
 //listeners
-theKeyPad.addEventListener('click', keyType, false)
+theKeyPad.addEventListener("click", keyType, false);
 
-//this function is called when a key is pressed (DIV)  it first checks if the calculationStarted flag is set to true or false, if it is false it then checks if the key press is a number, decimal point, negative/positive button, clear populates the currentNumber variable and the runningCalculation variable which is then displayed in the runningCalculation id in the html element
-//if (previousNumber == "") {console.log("gotit");
-//}
-//else console.log("dontgotit");
-
-//this object takes 2 numbers and the operation and returns the result of the calculation. This object is called when there is a previous number, a current number a previous operation present and either the = key or an operator key has been pressed
+//calculate object
 let operators = {
-    '+': (a, b) => a + b,
-    '-': (a, b) => a - b,
-    '*': (a, b) => a * b,
-    '÷': (a, b) => a / b
-},
-calculate = function (val1, val2, sign) {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    x: (a, b) => a * b,
+    "÷": (a, b) => a / b,
+  },
+  calculate = function (val1, val2, sign) {
     if (sign in operators) {
-        val1 = Number(val1);
-        val2 = Number(val2);
-        return operators[sign](val1, val2);
+      val1 = Number(val1);
+      val2 = Number(val2);
+      return operators[sign](val1, val2);
     }
+  };
+
+//what happens when a number is pressed
+function numberPressed(val) {
+  if (previousResult == null) {
+    if (val == ".") {
+        decimalPoint(val)
+      }
+      else if (val == "0") {
+        zeroKey(val);
+      }
+      else if (currentNumberDisplay.includes("%")) {
+      lastOperator = "x";
+      previousNumber = [];
+      previousNumberDisplay = [];
+      previousNumber.push(...currentNumber);
+      previousNumberDisplay.push(...currentNumberDisplay);
+      currentNumber.length=0;
+      currentNumberDisplay.length=0;
+      currentNumber.push(val);
+      currentNumberDisplay.push(val);
+      RebuildRunningCalc();
+
+    }
+      else {
+        currentNumber.push(val);
+        currentNumberDisplay.push(val);
+        previousResult = null;
+        calculationStarted = true;
+        RebuildRunningCalc();
+      }
 }
+   else { //if the user presses a number after a completed calculation, restart calculator and restart numbering
+    clearAll();
+    runningCalculation.push(val);
+    calculationStarted = true;
+    currentNumber.push(val);
+currentNumberDisplay.push(val);
+    RebuildRunningCalc()}
+   }
 
-// determines what happens when a number is pressed
-function numberPressed(val) { 
-    if (previousResult == null) {
-
-    if (val == ".") { 
-        if (currentNumber.length == 0) {
-            previousResult = null;
-            val = "0."
+//decimal point function block
+function decimalPoint(val) {
+    if (currentNumber.length == 0) {
+        previousResult = null;
+        val = "0.";
         runningCalculation.push(val);
         calculationStarted = true;
         currentNumber.push(val);
-        document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
+currentNumberDisplay.push(val);
+        RebuildRunningCalc();
     }
-    else if (currentNumber.includes(".") || currentNumber.includes("0.")) {
-        previousResult = null;}
-    else {runningCalculation.push(val);
-        previousResult = null;
+    else if (currentNumber.length == 1 && currentNumber[0] == "-") {
+      previousResult = null;
+        val = "0.";
+        runningCalculation.push(val);
         calculationStarted = true;
-    currentNumber.push(val);
-    document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
+        currentNumber.push(val);
+currentNumberDisplay.push(val);
+        RebuildRunningCalc();
+      } else if (currentNumber.includes(".") || currentNumber.includes("0.")) {
+        previousResult = null;
 }
-    }
-else {runningCalculation.push(val);
+else { 
+    runningCalculation.push(val);
     previousResult = null;
     document.getElementById("resultDisplay").innerHTML = "";
     calculationStarted = true;
-currentNumber.push(val);
-document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
-}
-    }
-    else {clearAll();
-        runningCalculation.push(val);
-        calculationStarted = true;
-        currentNumber.push(val);
-        document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
-    }
-
+    currentNumber.push(val);
+currentNumberDisplay.push(val);
+    RebuildRunningCalc();
+  }
 }
 
-
-// determines what happens when an operator is pressed
-function operatorPressed(operator) {
-    if (calculationStarted === false) {
-        alert("No number to calculate");
-}
-// this block determines what happens when it is the first operator that has been pressed
-    else if (previousNumber.length == 0 && lastOperator == null) {
-        previousResult = null;
-        document.getElementById("resultDisplay").innerHTML = "";
-        document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
-        runningCalculation.push(operator);
-        previousNumber = [];
-        previousNumber.push(...currentNumber);
-        currentNumber.length = 0;
-        lastOperator = operator;
-        document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
-    }
-
-// this block determines what happens when an operator is pressed after another operator has been pressed but there is nothing to calculate yet, basically it is just updating what operator the user has chosen
-    else if (lastOperator !== null && currentNumber.length === 0) {
-        previousResult = null;
-        runningCalculation.pop();
-        runningCalculation.push(operator);
-        lastOperator = operator;
-        document.getElementById("runningCalculation").innerHTML = runningCalculation.join('')
-    }
-//once all other cases have been passed, you can run the calculate function and update a bunch of variables
-    else {
-        arg1 = previousNumber.join('');
-        arg2 = currentNumber.join('');
-        answer = calculate(arg1, arg2, lastOperator);
-        runningCalculation = [];
-        runningCalculation.push(answer,operator);
-        document.getElementById("runningCalculation").innerHTML = runningCalculation.join('');
-        document.getElementById("resultDisplay").innerHTML = answer;
-        lastOperator = operator;
-        previousNumber = [];
-        previousNumber.push(answer);
-        currentNumber = [];
-    }
-
-}
-
-//called only when the equal key is pressed
-function equalKeyPressed() {
+// zero key function block
+function zeroKey(val) {
     if (currentNumber.length == 0) {
     }
-    else if (previousNumber.length !== 0 && lastOperator !== null) {
-        arg1 = previousNumber.join('');
-        arg2 = currentNumber.join('');
-        answer = calculate(arg1, arg2, lastOperator);
-        runningCalculation = [];
-        runningCalculation.push(answer);
-        document.getElementById("resultDisplay").innerHTML = '= ' + answer;
-        lastOperator = null;
-        previousResult = answer;
-        previousNumber = [];
-        currentNumber = [];
-        currentNumber.push(answer);
+    else if (currentNumber.length == 1 && currentNumber[0] == "-") {
+
     }
+else { 
+    runningCalculation.push(val);
+    previousResult = null;
+    document.getElementById("resultDisplay").innerHTML = "";
+    calculationStarted = true;
+    currentNumber.push(val);
+  currentNumberDisplay.push(val);
+    RebuildRunningCalc();
+  }
 }
 
-//determines which function to call based on key type
+// operator functions
+function operatorPressed(operator) {
+  if (calculationStarted === false) {
+    alert("No number to calculate");
+  }
+  // what happens when it is the first operator that has been pressed
+  else if (previousNumber.length == 0 && lastOperator == null) {
+    previousResult = null;
+    document.getElementById("resultDisplay").innerHTML = "";
+    RebuildRunningCalc();
+    previousNumber = [];
+    previousNumberDisplay = [];
+    previousNumber.push(...currentNumber);
+    previousNumberDisplay.push(...currentNumberDisplay);
+    currentNumber.length = 0;
+    currentNumberDisplay.length = 0;
+    lastOperator = operator;
+    RebuildRunningCalc();
+  }
+
+  // what happens when an operator is pressed after another operator has been pressed but there is nothing to calculate yet, basically it is just updating what operator the user has chosen
+  else if (lastOperator !== null && currentNumber.length === 0) {
+    previousResult = null;
+    runningCalculation.pop();
+    lastOperator = operator;
+    RebuildRunningCalc();
+  }
+  //once all other cases have been passed, you can run the calculate function and update a bunch of variables
+  else {
+    arg1 = previousNumber.join("");
+    arg2 = currentNumber.join("");
+    answer = calculate(arg1, arg2, lastOperator);
+    answer = (""+answer).split("")
+    runningCalculation.length = 0;
+    runningCalculation.splice(0, runningCalculation.length, ...answer);
+    runningCalculation.push(operator);
+    RebuildRunningCalc();
+    display = answer.join("");
+    if(display.length > 13) display = display.substring(0,13);
+    document.getElementById("resultDisplay").innerHTML = display;
+    lastOperator = operator;
+    previousNumber.length=0;
+    previousNumberDisplay.length=0;
+    previousNumber.splice(0,previousNumber.length, ...answer);
+    previousNumberDisplay.splice(0,previousNumber.length, ...answer);
+    currentNumber.length=0;
+    currentNumberDisplay.length=0;
+  }
+}
+
+//equal key function block 
+function equalKeyPressed() {
+  if (currentNumber.length == 0) {
+  } else if (previousNumber.length !== 0 && lastOperator !== null) {
+    arg1 = previousNumber.join("");
+    arg2 = currentNumber.join("");
+    answer = calculate(arg1, arg2, lastOperator);
+    answer = ("" + answer).split("");
+    runningCalculation.length = 0;
+    runningCalculation.splice(0,runningCalculation.length, ...answer);
+    display = answer.join("");
+    if(display.length > 12) display = display.substring(0,12);
+    document.getElementById("resultDisplay").innerHTML = "= " + display;
+    lastOperator = null;
+    previousResult = answer;
+    previousNumber.length=0;
+    previousNumberDisplay.length=0;
+    currentNumber.length = 0;
+    currentNumberDisplay.length = 0;
+    currentNumber.splice(0,answer.length, ...answer);
+    currentNumberDisplay.splice(0,answer.length, ...answer);
+  }
+}
+
+//backspace function block 
+function backspacePressed() {
+  //if (previousResult != null) {
+    //previousResult = null;
+    //document.getElementById("resultDisplay").innerHTML = "";
+    ///currentNumber.pop();
+    //currentNumberDisplay.pop();
+    //RebuildRunningCalc();
+  //}
+  //else 
+  if (runningCalculation.length == 0) {
+}
+  else if (lastOperator == null) {
+    runningCalculation.pop();
+    currentNumber.pop();
+    currentNumberDisplay.pop();
+    RebuildRunningCalc();
+  }
+  else if (currentNumber.length == 0 && lastOperator !== null) {
+    runningCalculation.pop();
+    currentNumber.push(...runningCalculation);
+    currentNumberDisplay.push(...runningCalculation);
+    previousNumber.length = 0;
+    previousNumberDisplay.length = 0;
+    lastOperator = null;
+    RebuildRunningCalc();
+      document.getElementById("resultDisplay").innerHTML ="";
+  }
+else if (currentNumber.length !== 0 && lastOperator !== null) {
+currentNumber.pop();
+currentNumberDisplay.pop();
+RebuildRunningCalc();
+document.getElementById("resultDisplay").innerHTML ="";
+}
+}
+
+//plus-minus keypress function block
+function plusMinusPressed() {
+  if (currentNumber[0] == "-") {
+    currentNumber.shift();
+    currentNumberDisplay.shift();
+RebuildRunningCalc();
+document.getElementById("resultDisplay").innerHTML ="";
+  }
+  else {currentNumber.unshift("-")
+  currentNumberDisplay.unshift("-");
+RebuildRunningCalc();
+document.getElementById("resultDisplay").innerHTML ="";
+}
+}
+
+//percentage key function block
+function percentagePressed() {
+  if (currentNumberDisplay[currentNumberDisplay.length-1] == "%") {
+    arg1 = currentNumber.join("");
+    currentNumber = calculate(arg1, "100","x");
+    currentNumber = (""+currentNumber).split("");
+    currentNumberDisplay.pop();
+RebuildRunningCalc();
+document.getElementById("resultDisplay").innerHTML ="";
+  }
+  else {
+    arg1 = currentNumber.join("");
+    currentNumber = calculate(arg1,"100","÷");
+    currentNumber = (""+currentNumber).split("");
+  currentNumberDisplay.push("%");
+RebuildRunningCalc();
+document.getElementById("resultDisplay").innerHTML ="";
+}
+}
+
+//Build the running calculation display and then print
+function RebuildRunningCalc(){
+runningCalculation.length=0;
+runningCalculation = runningCalculation.concat(previousNumberDisplay,lastOperator,currentNumberDisplay);
+display = runningCalculation.join("");
+if(display.length > 18) display = display.substring(0,18);
+document.getElementById("runningCalculation").innerHTML = display;
+document.getElementById("resultDisplay").innerHTML =""
+}
+
+//key identifier... switches function based on key pressed
 function keyType(e) {
-    if (e.target !== e.currentTarget) {
-        console.log(e.target);
-        let clickedItem = e.target.id;
-        if (clickedItem == 1 || clickedItem == 2 || clickedItem == 3 || clickedItem == 4 || clickedItem == 5 || clickedItem == 6 || clickedItem == 7 || clickedItem == 8 || clickedItem == 9 || clickedItem == 0 || clickedItem == '.') {
-    numberPressed(clickedItem);
-    console.log("number pressed " + clickedItem)
+  if (e.target !== e.currentTarget) {
+    console.log(e.target);
+    let clickedItem = e.target.id;
+    if (
+      clickedItem == 1 ||
+      clickedItem == 2 ||
+      clickedItem == 3 ||
+      clickedItem == 4 ||
+      clickedItem == 5 ||
+      clickedItem == 6 ||
+      clickedItem == 7 ||
+      clickedItem == 8 ||
+      clickedItem == 9 ||
+      clickedItem == 0 ||
+      clickedItem == "."
+    ) {
+      numberPressed(clickedItem);
+      console.log("number pressed " + clickedItem);
+    } else if (
+      clickedItem == "+" ||
+      clickedItem == "-" ||
+      clickedItem == "x" ||
+      clickedItem == "÷"
+    ) {
+      operatorPressed(clickedItem);
+      console.log("operator pressed");
+    } else if (clickedItem == "C") {
+      clearAll();
+    } else if (clickedItem == "=") {
+      equalKeyPressed();
     }
-    else if (clickedItem == "+" || clickedItem == "-" || clickedItem == "*" || clickedItem == "÷") {
-    operatorPressed(clickedItem);
-    console.log("operator pressed")
+    else if (clickedItem == "⌫") {
+      backspacePressed();
     }
-    else if (clickedItem == "C") {
-        clearAll();
+    else if (clickedItem == "⁺∕₋") {
+      plusMinusPressed();
     }
-    else if (clickedItem == "=") {
-        equalKeyPressed();
+    else if (clickedItem == "%") {
+      percentagePressed();
     }
     e.stopPropagation();
-}
+  }
 }
 
 function clearAll() {
-    currentNumber.length = 0;
-    previousNumber.length = 0;
-    runningCalculation.length = 0;
-    calculationStarted = false;
-    previousResult = null;
-    lastOperator = null;
-    document.getElementById("runningCalculation").innerHTML = runningCalculation.join('');
-    document.getElementById("resultDisplay").innerHTML = "";
-
+  currentNumber.length = 0;
+  currentNumberDisplay.length = 0;
+  previousNumber.length = 0;
+  previousNumberDisplay.length = 0;
+  runningCalculation.length = 0;
+  calculationStarted = false;
+  previousResult = null;
+  lastOperator = null;
+  document.getElementById("runningCalculation").innerHTML =
+    runningCalculation.join("");
+  document.getElementById("resultDisplay").innerHTML = "";
 }
-
